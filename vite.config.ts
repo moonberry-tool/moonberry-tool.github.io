7d663bf3a14d04366ad3e17394200f76fa14f17c
moonberry-tool <anotherbeginning5@gmail.com>
2026-08-19 23:56:20 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/moonberry/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
